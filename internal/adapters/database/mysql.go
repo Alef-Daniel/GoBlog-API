24c1a51059ac5a3e860558c80eb666c410b4a671
Alef Sousa Aguiar Daniel <83924026+Alef-Daniel@users.noreply.github.com>
2026-01-29 00:07:57 -0300

    Feature/init config db (#2)
    * feat: add dependencies mysql lib
    
    * feat: create interface database
    
    * feat: create implementation mysql to interface database
    
    * feat: add lib golang-migrate to execute migrations
    
    * feat: add news files migrations
    
    * feat: create connection retrieve to mysql driver
    
    * feat: initialize db and execute migrations automatic.

---
+++ internal/adapters/database/mysql.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MySQL struct {
+	conn *sql.DB
+}
+
+func NewMySQL() (*MySQL, error) {
+	dsn := os.Getenv("DB_DSN")
+
+	if dsn == "" {
+		return nil, fmt.Errorf("env DB_DSN not found")
+	}
+
+	conn, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	err = conn.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	return &MySQL{conn: conn}, nil
+}
+
+func (m *MySQL) Close() error {
+	err := m.conn.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close connection: %w", err)
+	}
+
+	return nil
+}
+
+func (m *MySQL) GetConnection() *sql.DB {
+	return m.conn
+}
