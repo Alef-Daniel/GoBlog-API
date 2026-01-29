c6632a914358b42b9bd88cf54231933c8e817742
Alef-Daniel <alef.adaniel@outlook.com>
2026-01-29 00:06:39 -0300

    feat: initialize db and execute migrations automatic.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/goblog-api/internal/adapters/database"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
+	_ "github.com/golang-migrate/migrate/source/file"
+)
 
 func main() {
 	fmt.Println("initializing project GoBlog API")
+
+	db, err := database.NewMySQL()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(db.GetConnection(), &mysql.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"mysql", driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = m.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
